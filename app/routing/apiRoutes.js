feb7ea3431770e2fbe8c75047ce2b277bd4966cd
AJ Ford <AJCranford1019@gmail.com>
2017-04-26 16:38:37 -0400

    everything but match logic

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+const friends = require("../data/friends.js");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+
+  app.post("/api/friends", function(req, res) {
+    friends.push(req.body);
+      res.json(true);
+  });
+
+};
+
+
+//
+//   app.post("/api/clear", function() {
+//     // Empty out the arrays of data
+//     tableData = [];
+//     waitListData = [];
+//
+//     console.log(tableData);
+//   });
+// };
